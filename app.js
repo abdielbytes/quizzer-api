5295019968807b18cfaf7e413dbd35f2980a0fb1
wilsonabdiel <wilsonabdiel86@gmail.com>
2024-08-08 21:49:42 +0100

    works so far

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// app.js
 const express = require('express');
+const cors = require('cors'); // Import the CORS middleware
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
@@ -10,10 +10,23 @@ dotenv.config();
 
 const app = express();
 
+// Connect to the database
 connectDB();
 
+// Configure CORS options
+const corsOptions = {
+  origin: 'http://localhost:8080', // Allow requests only from this origin
+  methods: 'GET,POST,PUT,DELETE', // Allow specific HTTP methods
+  allowedHeaders: 'Content-Type,Authorization' // Allow specific headers
+};
+
+// Enable CORS with options
+app.use(cors(corsOptions));
+
+// Middleware
 app.use(bodyParser.json());
 
+// Define Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/quizzes', quizRoutes);
 
