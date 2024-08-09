5a163e1c4c77c34db573e07926a58f4198af277f
wilsonabdiel <wilsonabdiel86@gmail.com>
2024-08-09 07:18:53 +0100

    feat:get quiz

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -1,4 +1,3 @@
-// controllers/quizController.js
 const Quiz = require('../models/Quiz');
 
 exports.createQuiz = async (req, res) => {
@@ -17,6 +16,20 @@ exports.getQuizzes = async (req, res) => {
   try {
     const quizzes = await Quiz.find();
     res.json(quizzes);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.getQuizById = async (req, res) => {
+  try {
+    const quiz = await Quiz.findById(req.params.id);
+
+    if (!quiz) {
+      return res.status(404).json({ error: 'Quiz not found' });
+    }
+
+    res.json(quiz);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
